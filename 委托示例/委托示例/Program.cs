b1b1bc70c2972ee5bfbec2c9ecbb837d25e6cf0e
hj <786762595@qq.com>
2018-09-15 16:05:11 +0800

    delegate示例

---
+++ 委托示例/委托示例/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 委托示例
+{
+    delegate double DoubleOp(double x); //声明委托
+    class MathOperations
+    {
+        public static double MultiplyByTwo(double value)
+        {
+            return value * 2;
+        }
+        public static double Square(double value)
+        {
+            return value * value;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoubleOp[] operations ={
+                                                                                    MathOperations.MultiplyByTwo,
+                                                                                    MathOperations.Square
+                                   };//委托实例设置 
+            for (int i = 0; i < operations.Length; i++)
+            {
+                Console.WriteLine("Using operations[{0}]:",i);
+                ProcessAddDisplayNumber(operations[i], 3.5);
+                
+                //Console.WriteLine();
+               
+            }
+            Console.ReadKey();
+        }
+        static void ProcessAddDisplayNumber(DoubleOp action, double value)
+        {
+            double result = action(value);
+            Console.WriteLine("Value is {0},result of operation is {1}",value,result);
+        }
+    }
+}
---
+++ 控制台项目/控制台项目/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Xml;
+
+namespace 控制台项目
+{
+    class ExecuteXmlReaderExample
+    {
+        static void Main(string[] args)
+        {
+            string constr = "data source = 172.30.2.31; initial catalog = itcast2018; integrated security = SSPI";
+
+            SqlConnection conn = new SqlConnection(constr);
+            conn.Open();
+            string select = "select name,age from staff FOR XML AUTO";
+            SqlCommand cmd = new SqlCommand(select, conn);
+            XmlReader xl = cmd.ExecuteXmlReader();
+            xl.Read();
+            string data;
+            do
+            {
+                data = xl.ReadOuterXml();
+                if (!string.IsNullOrEmpty(data))
+                {
+                    Console.WriteLine(data);
+                }
+            } while (!string.IsNullOrEmpty(data));
+            conn.Close();
+            Console.ReadLine();
+        }
+    }
+}
